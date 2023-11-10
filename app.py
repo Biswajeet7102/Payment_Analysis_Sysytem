141f747a5b0282093f5cc542f922e95108fba2e5
Biswajeet <biswajeet1702@gmail.comcls>
2023-11-10 17:08:51 +0530

    bar code scanner added

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
-
-
+from flask import Flask, render_template, request, jsonify
 import fasttext
+import re
+from flask_cors import CORS
 
 app = Flask(__name__)
-model = fasttext.load_model("C:\Payment_Type_Classifiation\model.bin")  # Double backslashes in the path
+CORS(app)
+model = fasttext.load_model("C:\Payment_Type_Classifiation\model.bin")
 
 @app.route('/')
 def home():
@@ -13,10 +14,22 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        data = request.form.get('data')  
+        data = request.get_json().get('data')  # Extract data from JSON in the request body
         print(data)
+        
         result = model.predict(data)
-        return render_template('index.html', result=result)
+        print(result)
+        
+        pattern = r"\(\('__label__(\w+)\'.*?\)"
+        match = re.search(pattern, str(result))  # Convert result to string before applying regex
+        
+        if match:
+            label = match.group(1)
+            result = label
+        else:
+            print("No match found.")
+
+        return jsonify({'prediction': result})  # Return prediction as JSON
 
 if __name__ == '__main__':
     app.run(debug=True)
