8c24bf87872f520f4d761a2f652e4b0eab376018
Biswajeet <biswajeet1702@gmail.comcls>
2023-11-11 15:04:58 +0530

    sql connected

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+# app.py
 from flask import Flask, render_template, request, jsonify
 import fasttext
 import re
 from flask_cors import CORS
+from flask_mysqldb import MySQL
+from db_config import DATABASE_CONFIG  # assuming you have a separate file for database configuration
 
 app = Flask(__name__)
 CORS(app)
 model = fasttext.load_model("C:\Payment_Type_Classifiation\model.bin")
+
+app.config['MYSQL_HOST'] = DATABASE_CONFIG['host']
+app.config['MYSQL_USER'] = DATABASE_CONFIG['user']
+app.config['MYSQL_PASSWORD'] = DATABASE_CONFIG['password']
+app.config['MYSQL_DB'] = DATABASE_CONFIG['db']
+app.config['MYSQL_SSL_CA'] = DATABASE_CONFIG['ssl_ca']
+
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
@@ -15,7 +26,7 @@ def home():
 def predict():
     if request.method == 'POST':
         data = request.get_json().get('data')  # Extract data from JSON in the request body
-        amount= request.get_json().get('amount')
+        amount = request.get_json().get('amount')
         print(data)
         
         result = model.predict(data)
@@ -30,7 +41,17 @@ def predict():
         else:
             print("No match found.")
 
-        return jsonify({'prediction': result,'amount':amount})  # Return prediction as JSON
+        try:
+            cursor = mysql.connection.cursor()
+            cursor.execute("INSERT INTO payment_history (Prediction, Amount) VALUES (%s, %s)", (result, amount))
+            mysql.connection.commit()
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            if 'cursor' in locals():
+                cursor.close()
+
+        return jsonify({'prediction': result, 'amount': amount})  # Return prediction as JSON
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=5000)
---
+++ db_config.py
@@ -0,0 +1,8 @@
+# db_config.py
+DATABASE_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'sinu172002',
+    'db': 'sys',
+    'ssl_ca': None
+}
