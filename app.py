be9948c55e9f49e7b74098f676208eabbfe15295
Biswajeet <biswajeet1702@gmail.comcls>
2023-11-12 23:08:58 +0530

    all the features has been added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # app.py
 from flask import Flask, render_template, request, jsonify
 import fasttext
+import json
 import re
-import datetime
+from datetime import datetime
+from decimal import Decimal
 from flask_cors import CORS
 from flask_mysqldb import MySQL
 from db_config import DATABASE_CONFIG  # assuming you have a separate file for database configuration
@@ -21,7 +23,19 @@ mysql = MySQL(app)
 
 @app.route('/')
 def home():
+    return render_template('home.html')
+
+@app.route('/index.html')
+def index():
     return render_template('index.html')
+
+@app.route('/analysis.html')
+def analysis():
+    return render_template('analysis.html')
+
+
+
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -44,7 +58,9 @@ def predict():
 
         try:
             cursor = mysql.connection.cursor()
-            date = datetime.datetime.now()
+            date = datetime.now()
+            print(date)
+
             cursor.execute("INSERT INTO payment_history (Prediction, Amount, Date) VALUES (%s, %s, %s)", (result, amount,date))
             mysql.connection.commit()
             print("Success")
@@ -56,6 +72,62 @@ def predict():
             
 
         return jsonify({'prediction': result, 'amount': amount})  # Return prediction as JSON
+    
+
+@app.route('/analysis_pred', methods=['POST'])
+
+
+    
+def analysis_pred():
+    if request.method == 'POST':
+        data = request.get_json()  # Extract JSON data from the request body
+        print("inside /analysis_pred")
+
+        # Extract start and end dates from the JSON data
+        print(data)
+        start_date = data.get('startDate')
+        end_date = data.get('endDate')
+        print(start_date)
+        print(end_date)
+        start_date = datetime.strptime(start_date, "%Y-%m-%d")
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+        start_date = start_date.date()
+        end_date = end_date.date()
+        print(start_date)
+        print(end_date)
+
+        # SQL query to sum amounts for each prediction between the given start and end dates
+        sql_query = """
+            SELECT Prediction, SUM(Amount) as TotalAmount
+            FROM payment_history
+            WHERE Date BETWEEN %s AND %s
+            GROUP BY Prediction;
+        """
+        print("Query framed")
+        try:
+            cursor = mysql.connection.cursor()
+
+            # Execute the query with the start and end dates as parameters
+            cursor.execute(sql_query, (start_date, end_date))
+
+
+            # Fetch the result
+            result = cursor.fetchall()
+            print(result)
+            # result = {'category': [], 'amount': []}
+            data_list = [{'category': item[0], 'amount': float(item[1])} for item in result]
+            print(data_list)
+            json_data = json.dumps(data_list)
+            return json_data
+        
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return jsonify({'error': 'An error occurred during analysis'})
+
+        finally:
+            if 'cursor' in locals():
+                cursor.close()
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
